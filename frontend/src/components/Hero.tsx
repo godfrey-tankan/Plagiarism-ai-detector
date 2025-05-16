d50df83b031da434e7b2dfcbd9faeee183364b87
godfrey-tankan <godfreytantswakandeya@gmail.com>
2025-05-16 17:38:28 +0200

    feat: Update Hero component to navigate users based on type and enhance document analysis display

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,7 +1,10 @@
 
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 const Hero = () => {
   const userType = localStorage.getItem('user_type');
+  const navigate = useNavigate();
   return (
     <div className="bg-gradient-to-r from-white-600 to-teal-700 text-black py-16">
       <div className="container mx-auto px-6 flex flex-col md:flex-row items-center">
@@ -21,7 +24,13 @@ const Hero = () => {
           <div className="flex flex-col sm:flex-row justify-center md:justify-start w-full">
             <Button
               className="py-6 px-8 bg-teal-700 text-white hover:bg-teal-600 rounded-lg font-bold mb-4 sm:mb-0 sm:mr-4"
-              onClick={() => document.getElementById('upload-section')?.scrollIntoView({ behavior: 'smooth' })}
+              onClick={() => {
+                if (userType === 'student') {
+                  navigate('/history');
+                } else {
+                  document.getElementById('upload-section')?.scrollIntoView({ behavior: 'smooth' })
+                }
+              }}
             >
               {userType !== 'student' ? 'Upload Document' : 'View Document Metrics'}
             </Button>
@@ -33,27 +42,57 @@ const Hero = () => {
           </div>
         </div>
 
+        {
+          userType !== 'student' ?
+            <>
 
-        <div className="w-full py-6 flex justify-center">
-          <div className="w-full max-w-2xl bg-white bg-opacity-10 rounded-xl backdrop-filter backdrop-blur-md border border-white border-opacity-20 shadow-2xl flex flex-col md:flex-row items-center justify-center animate-slide-up p-4">
-            <div className="p-4 md:p-8 text-center flex-1">
-              <div className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-teal-600">
-                99.8%
+              <div className="w-full py-6 flex justify-center">
+                <div className="w-full max-w-2xl bg-white bg-opacity-10 rounded-xl backdrop-filter backdrop-blur-md border border-white border-opacity-20 shadow-2xl flex flex-col md:flex-row items-center justify-center animate-slide-up p-4">
+                  <div className="p-4 md:p-8 text-center flex-1">
+                    <div className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-teal-600">
+                      99.8%
+                    </div>
+                    <p className="text-sm md:text-lg">Detection Accuracy</p>
+                  </div>
+
+                  <div className="hidden md:block border-r border-white border-opacity-20 h-20 mx-2 md:mx-6"></div>
+                  <div className="md:hidden w-full border-t border-white border-opacity-20 my-4"></div>
+
+                  <div className="p-4 md:p-8 text-center flex-1">
+                    <div className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-teal-600">
+                      3M+
+                    </div>
+                    <p className="text-sm md:text-lg">Documents Analyzed</p>
+                  </div>
+                </div>
               </div>
-              <p className="text-sm md:text-lg">Detection Accuracy</p>
-            </div>
+            </>
+            :
+            <>
+              <div className="w-full py-6 flex justify-center">
+                <div className="w-full max-w-2xl bg-white bg-opacity-10 rounded-xl backdrop-filter backdrop-blur-md border border-white border-opacity-20 shadow-2xl flex flex-col md:flex-row items-center justify-center animate-slide-up p-4">
+                  <div className="p-4 md:p-8 text-center flex-1">
+                    <div className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-teal-600">
+                      99.8%
+                    </div>
+                    <p className="text-sm md:text-lg">Detection Accuracy</p>
+                  </div>
 
-            <div className="hidden md:block border-r border-white border-opacity-20 h-20 mx-2 md:mx-6"></div>
-            <div className="md:hidden w-full border-t border-white border-opacity-20 my-4"></div>
+                  <div className="hidden md:block border-r border-white border-opacity-20 h-20 mx-2 md:mx-6"></div>
+                  <div className="md:hidden w-full border-t border-white border-opacity-20 my-4"></div>
 
-            <div className="p-4 md:p-8 text-center flex-1">
-              <div className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-teal-600">
-                3M+
+                  <div className="p-4 md:p-8 text-center flex-1">
+                    <div className="text-3xl md:text-5xl font-bold mb-2 md:mb-4 text-teal-600">
+                      30+
+                    </div>
+                    <p className="text-sm md:text-lg">Documents Analyzed per day</p>
+                  </div>
+                </div>
               </div>
-              <p className="text-sm md:text-lg">Documents Analyzed</p>
-            </div>
-          </div>
-        </div>
+            </>
+        }
+
+
       </div>
     </div>
   );
